7998334ed40cd08ad5f122c21e1d5fa260cfca99
Shubha Rajan <shubharajan94@gmail.com>
2019-04-14 17:19:39 -0700

    method passes all tests and properly handles edge cases

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -8,5 +8,23 @@
 # Time complexity: ?
 # Space complexity: ?
 def fibonacci(n)
-  raise NotImplementedError
+  if n && n >= 0
+    current = 0
+    one_before = 0
+    two_before = 0
+    i = 0
+    while i <= n
+      if i == 1
+        current = 1
+      else
+        two_before = one_before
+        one_before = current
+        current = one_before + two_before
+      end
+      i += 1
+    end
+    return current
+  else
+    raise ArgumentError, "n must be an integer greater than 0"
+  end
 end
